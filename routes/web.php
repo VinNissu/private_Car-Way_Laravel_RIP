4ef7b40921ace11919554dc159f4bff7e21ddc75
marcocarneiro <110014684+VinnyAlmeidaBR@users.noreply.github.com>
2022-11-24 12:51:04 -0200

    Update

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,12 @@ use Illuminate\Support\Facades\Route;
     Route::get('/feed', function () {
     return view('feed');
     })->name('feed');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
